9a799cb22eee34916fd9641e0667c788c71f498c
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-09-17 16:56:10 +0300

    Dynamic Routing
    Dynamic Routing

---
+++ src/app/companies/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from './components/header';
+import Toolbar from './components/toolbar';
+import SearchInput from './components/search-input';
+import AddCompanyButton from '../components/AddCompanyButton/AddCompanyButton';
+import CompanyTable from './components/company-table';
+import CompanyRow from './components/company-row';
+import { Status } from '../components/Label/Label';
+
+const Page = () => {
+  return (
+    <>
+      <Header>Companies</Header>
+      <Toolbar action={<AddCompanyButton />}>
+        <SearchInput />
+      </Toolbar>
+      <CompanyTable>
+        <CompanyRow
+          id={1}
+          category={'Products'}
+          company={'CostCo'}
+          status={Status.Active}
+          promotion={true}
+          country={'USA'}
+          joinedDate={'12.02.2024'}
+        />
+      </CompanyTable>
+    </>
+  );
+};
+
+export default Page;
